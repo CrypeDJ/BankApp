22bbaabc24f06464c3b5d08feacf849cedcbd042
CrypeDJ <litowrog@gmail.com>
2024-06-26 18:54:17 +0300

    Screen Add

--- app/src/main/java/com/crype/bankapp/MainActivity.kt
+++ app/src/main/java/com/crype/bankapp/MainActivity.kt
@@ -6,15 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.crype.bankapp.presentation.screen.HomeScreen
+import com.crype.bankapp.presentation.screen.AddScreen
 import com.crype.bankapp.ui.theme.BankAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen(
-                        accountName = "Saving account",
-                        numberOfAccount = "32509235032",
-                        lastNumbersOfCard = "•••• 1234",
-                        senderName = "OOO “Company”",
-                        date = "01.02.2003",
-                        transactionProgress = "Executed",
-                        money = "10.09",
-                        itemsCount = 4
-                    )
+                    AddScreen()
 
                 }
             }
